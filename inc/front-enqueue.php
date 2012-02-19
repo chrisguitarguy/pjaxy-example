521ccb70173f97bee37dbba1cae92bf5c1e1561e
Christopher Davis <cdavis9999@gmail.com>
2012-02-18 23:48:42 -0600

    Fixing some deleted files issues

--- inc/front-enqueue.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'pjaxy_front_enqueue_scripts' );
-function pjaxy_front_enqueue_scripts()
-{
-    if( is_singular() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
-}
